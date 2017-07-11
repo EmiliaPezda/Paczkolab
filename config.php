31b1d0d4fff4ae6d93ccad0cdc683777fa8c6b18
Emilia Pezda <emilia.ciejka@gmail.com>
2017-07-11 20:35:16 +0100

    fixing problems with connection

--- config.php
+++ config.php
@@ -2,31 +2,16 @@
 include_once 'class/DBConn.php';
 include_once 'class/abstract/DB.php';
 
-
-
-$dsn = '';
-
 $servername = "localhost";
 $username = "root";
 $password = "coderslab";
 $baseName = "Paczkolab";
-//
-//$conn = new mysqli($servername, $username, $password, $baseName);
-//
-//if ($conn->connect_error) {
-//    echo "Connection failed. Error: " . $conn->connect_error;
-//    die;
-//}
-//
-//$setEncodingSql = "SET CHARSET utf8";
-//$conn->query($setEncodingSql);
 
-$conn = new DBConn($dsn, $password, $username);
+$dsn = "mysql:host=$servername;dbname=$baseName;charset=utf8";
 
 
+$conn = new DBConn($dsn, $username, $password);
 
-$setEncodingSql = "SET CHARSET utf8";
-$conn->query($setEncodingSql);
 
 
 DB::$conn = $conn;
